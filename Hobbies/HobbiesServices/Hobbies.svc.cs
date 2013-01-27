b6688a2692e78fac48c48ccd038474a1a5e54b4d
Luis Caiguaraico <luiscm0609@gmail.com>
2013-01-27 18:55:26 -0500

    Firts commit

---
+++ Hobbies/HobbiesServices/Hobbies.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HobbiesServices
+{
+    public class Hobbies : IHobbies
+    {
+
+        public void registrarServicio(int codigo, string descripcion)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
